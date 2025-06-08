f299f32c6840844a8622c95a005e2afc2ee22416
Lunatikera <carlitosjuven@gmail.com>
2025-06-07 22:06:12 -0700

    update

--- GuesstheNumber/app/src/main/java/rios/carlos/guessthenumber/MainActivity.kt
+++ GuesstheNumber/app/src/main/java/rios/carlos/guessthenumber/MainActivity.kt
@@ -2,51 +2,97 @@ package rios.carlos.guessthenumber
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import rios.carlos.guessthenumber.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private var number = 0
-    private val targetNumber = 42
+    var minValue = 0
+    var maxValue = 100
+    var num: Int = 0
+    var won = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val guessings: TextView = findViewById(R.id.textViewBelow)
+        val down: TextView = findViewById(R.id.down)
+        val up: TextView = findViewById(R.id.up)
+        val generate: TextView = findViewById(R.id.generate)
+        val guessed: TextView = findViewById(R.id.guessed)
 
-        binding.textViewBelow.text = "Current number: $number"
+        guessings.text = "Toca generar para empezar"
+        guessed.visibility = View.GONE
+        up.isEnabled = false
+        down.isEnabled = false
 
-        binding.down.setOnClickListener {
-            number--
-            binding.textViewBelow.text = "Current number: $number"
-            checkGuess()
+        generate.setOnClickListener {
+            num = Random.nextInt(minValue, maxValue)
+            guessings.text = num.toString()
+            generate.visibility = View.INVISIBLE
+            guessed.visibility = View.VISIBLE
+            up.isEnabled = true
+            down.isEnabled = true
         }
 
-        binding.up.setOnClickListener {
-            number++
-            binding.textViewBelow.text = "Current number: $number"
-            checkGuess()
+        up.setOnClickListener {
+            minValue = num + 1
+            if (checkingLimits()) {
+                num = Random.nextInt(minValue, maxValue)
+                guessings.text = num.toString()
+            } else {
+                guessings.text = "¡No puede ser! Ganaste :("
+                up.isEnabled = false
+                down.isEnabled = false
+                guessed.text = "Volver a jugar"
+                won = true
+            }
         }
 
-        binding.generate.setOnClickListener {
-            binding.anotherButton.visibility = View.GONE
-            number = 0
-            binding.textViewBelow.text = "Current number: $number"
+        down.setOnClickListener {
+            maxValue = num - 1
+            if (checkingLimits()) {
+                num = Random.nextInt(minValue, maxValue)
+                guessings.text = num.toString()
+            } else {
+                guessings.text = "¡No puede ser! Ganaste :("
+                up.isEnabled = false
+                down.isEnabled = false
+                guessed.text = "Volver a jugar"
+                won = true
+            }
         }
-        binding.anotherButton.setOnClickListener {
-            binding.anotherButton.visibility = View.GONE
-            Toast.makeText(this, "¡Adivinaste correctamente!", Toast.LENGTH_SHORT).show()
+
+        guessed.setOnClickListener {
+            if (!won) {
+                guessings.text = "Adiviné, tu número es el $num"
+                guessed.text = "Volver a jugar"
+                up.isEnabled = false
+                down.isEnabled = false
+                won = true
+            } else {
+                resetValues()
+                guessings.text = "Toca generar para empezar"
+                guessed.text = "Guessed"
+                guessed.visibility = View.GONE
+                generate.visibility = View.VISIBLE
+                up.isEnabled = false
+                down.isEnabled = false
+            }
         }
     }
 
+    private fun resetValues() {
+        minValue = 0
+        maxValue = 100
+        num = 0
+        won = false
+    }
 
-    private fun checkGuess() {
-        if (number == targetNumber) {
-            binding.anotherButton.visibility = View.VISIBLE 
-        }
+    private fun checkingLimits(): Boolean {
+        return (maxValue - minValue) > 0
     }
 }
